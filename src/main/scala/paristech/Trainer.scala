bb7d378425bdefe86361b7484dfa7452159da878
antonindurieux <antonin.durieux@gmail.com>
2019-11-15 23:48:23 +0100

    Trainer completed

--- src/main/scala/paristech/Trainer.scala
+++ src/main/scala/paristech/Trainer.scala
@@ -2,11 +2,12 @@ package paristech
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
-import org.apache.spark.ml.classification.LogisticRegression
-import org.apache.spark.ml.feature._
-import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
+import org.apache.spark.ml.feature._
+import org.apache.spark.ml.tuning.{ParamGridBuilder, TrainValidationSplit}
+import org.apache.spark.sql.SparkSession
 
 
 
@@ -51,33 +52,33 @@ object Trainer {
 
     println("Trainer launched")
 
-    // Chargement du DataFrame
+    // DataFrame loading
     val data = spark.read.parquet("/home/antonin/Dropbox/Cours_Paristech/INF729-Introduction_Hadoop/Spark/cours-spark-telecom-master/data/prepared_trainingset/*.parquet")
 
-    // Split en training/test set
+    // Training/test set splitting
     val Array(trainingData, testData) = data.randomSplit(Array(0.9, 0.1))
 
     println("Pipeline building...")
 
     // Stage 1 : Récupération des mots des textes
-    val tokenizer = new RegexTokenizer()
+    val text_tokenizer = new RegexTokenizer()
       .setPattern("\\W+")
       .setGaps(true)
       .setInputCol("text")
       .setOutputCol("tokens")
 
     // Stage 2 : Suppression des stop words
-    val remover = new StopWordsRemover()
+    val stopword_remover = new StopWordsRemover()
       .setInputCol("tokens")
       .setOutputCol("filtered")
 
     // Stage 3 : Calcul de la partie TF (transformation des mots en vecteurs de comptage)
-    val cv_text = new CountVectorizer()
+    val wordtoken_vectorizer = new CountVectorizer()
       .setInputCol("filtered")
       .setOutputCol("rawFeatures")
 
     // Stage 4 : Calcul de la partie IDF
-    val idf = new IDF()
+    val wordvector_idf = new IDF()
       .setInputCol("rawFeatures")
       .setOutputCol("tfidf")
 
@@ -94,17 +95,17 @@ object Trainer {
       .setHandleInvalid("keep")
 
     // Stage 7/8 : One-Hot encoding de country2 et currency2
-    val encoder = new OneHotEncoderEstimator()
+    val onehot_encoder = new OneHotEncoderEstimator()
       .setInputCols(Array("country_indexed", "currency_indexed"))
       .setOutputCols(Array("country_onehot", "currency_onehot"))
 
     // Stage 9 : Assemblage des features en un unique vecteur
-    val assembler = new VectorAssembler()
+    val feature_assembler = new VectorAssembler()
       .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "country_onehot", "currency_onehot"))
       .setOutputCol("features")
 
     // Stage 10 : Création et instanciation du modèle de classification
-    val lr = new LogisticRegression()
+    val logistic_reg = new LogisticRegression()
       .setElasticNetParam(0.0)
       .setFitIntercept(true)
       .setFeaturesCol("features")
@@ -118,24 +119,39 @@ object Trainer {
 
     // Création du pipeline
     val pipeline = new Pipeline()
-      .setStages(Array(tokenizer, remover, cv_text, idf, country_indexer, currency_indexer, encoder, assembler, lr))
+      .setStages(Array(text_tokenizer, stopword_remover, wordtoken_vectorizer, wordvector_idf,
+        country_indexer, currency_indexer, onehot_encoder, feature_assembler, logistic_reg))
 
-    println("Model fitting...")
+    // Parameters grid to search over
+    val paramGrid = new ParamGridBuilder()
+      .addGrid(logistic_reg.elasticNetParam, Array(10e-8, 10e-6, 10e-4, 10e-2))
+      .addGrid(wordtoken_vectorizer.minDF, Array(55.0, 75.0, 95.0))
+      .build()
 
-    // Entrainement du modèle
-    val model = pipeline.fit(trainingData)
-
-    println("Prediction on test data...")
-
-    // Prédictions sur les données test
-    val dfWithSimplePredictions = model.transform(testData)
-    dfWithSimplePredictions.groupBy("final_status", "predictions").count.show()
-
+    // Performance evaluation
     val evaluator = new MulticlassClassificationEvaluator()
       .setLabelCol("final_status")
       .setPredictionCol("predictions")
       .setMetricName("f1")
-    val f1_score = evaluator.evaluate(dfWithSimplePredictions)
+
+    // Train-validation split
+    val trainValidationSplit = new TrainValidationSplit()
+      .setEstimator(pipeline)
+      .setEvaluator(evaluator)
+      .setEstimatorParamMaps(paramGrid)
+      .setTrainRatio(0.7)
+      .setParallelism(2)
+
+    // Model fitting
+    println("Model fitting...")
+    val model = trainValidationSplit.fit(trainingData)
+
+    // Predictions on test data
+    println("Computing predictions on test data...")
+    val dfWithPredictions = model.transform(testData)
+    dfWithPredictions.groupBy("final_status", "predictions").count.show()
+
+    val f1_score = evaluator.evaluate(dfWithPredictions)
     println(s"F1 Score = $f1_score")
 
   }
