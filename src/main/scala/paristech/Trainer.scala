437a2e225d733cf583aeffbf50e567adc7d73156
antonindurieux <antonin.durieux@gmail.com>
2019-11-15 22:54:11 +0100

    Pipeline & prédictions finished

--- src/main/scala/paristech/Trainer.scala
+++ src/main/scala/paristech/Trainer.scala
@@ -2,8 +2,11 @@ package paristech
 
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
+import org.apache.spark.ml.classification.LogisticRegression
 import org.apache.spark.ml.feature._
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.ml.Pipeline
+import org.apache.spark.ml.evaluation.MulticlassClassificationEvaluator
 
 
 
@@ -33,7 +36,6 @@ object Trainer {
       .appName("TP Spark : Trainer")
       .getOrCreate()
 
-
     /*******************************************************************************
       *
       *       TP 3
@@ -47,10 +49,15 @@ object Trainer {
       *
       ********************************************************************************/
 
-    println("hello world ! from Trainer")
+    println("Trainer launched")
 
     // Chargement du DataFrame
     val data = spark.read.parquet("/home/antonin/Dropbox/Cours_Paristech/INF729-Introduction_Hadoop/Spark/cours-spark-telecom-master/data/prepared_trainingset/*.parquet")
+
+    // Split en training/test set
+    val Array(trainingData, testData) = data.randomSplit(Array(0.9, 0.1))
+
+    println("Pipeline building...")
 
     // Stage 1 : Récupération des mots des textes
     val tokenizer = new RegexTokenizer()
@@ -64,19 +71,72 @@ object Trainer {
       .setInputCol("tokens")
       .setOutputCol("filtered")
 
-    // Stage 3 : Calcul de la partie TF
-    val cvModel: CountVectorizerModel = new CountVectorizer()
+    // Stage 3 : Calcul de la partie TF (transformation des mots en vecteurs de comptage)
+    val cv_text = new CountVectorizer()
       .setInputCol("filtered")
-      .setOutputCol("features")
-      .setVocabSize(3)
-      .setMinDF(2)
-      .fit(data)
+      .setOutputCol("rawFeatures")
 
     // Stage 4 : Calcul de la partie IDF
+    val idf = new IDF()
+      .setInputCol("rawFeatures")
+      .setOutputCol("tfidf")
 
+    // Stage 5 : Conversion de country2 en quantité numérique
+    val country_indexer = new StringIndexer()
+      .setInputCol("country2")
+      .setOutputCol("country_indexed")
+      .setHandleInvalid("keep")
 
+    // Stage 6 : Conversion de currency2 en quantité numérique
+    val currency_indexer = new StringIndexer()
+      .setInputCol("currency2")
+      .setOutputCol("currency_indexed")
+      .setHandleInvalid("keep")
 
+    // Stage 7/8 : One-Hot encoding de country2 et currency2
+    val encoder = new OneHotEncoderEstimator()
+      .setInputCols(Array("country_indexed", "currency_indexed"))
+      .setOutputCols(Array("country_onehot", "currency_onehot"))
 
+    // Stage 9 : Assemblage des features en un unique vecteur
+    val assembler = new VectorAssembler()
+      .setInputCols(Array("tfidf", "days_campaign", "hours_prepa", "goal", "country_onehot", "currency_onehot"))
+      .setOutputCol("features")
+
+    // Stage 10 : Création et instanciation du modèle de classification
+    val lr = new LogisticRegression()
+      .setElasticNetParam(0.0)
+      .setFitIntercept(true)
+      .setFeaturesCol("features")
+      .setLabelCol("final_status")
+      .setStandardization(true)
+      .setPredictionCol("predictions")
+      .setRawPredictionCol("raw_predictions")
+      .setThresholds(Array(0.7, 0.3))
+      .setTol(1.0e-6)
+      .setMaxIter(20)
+
+    // Création du pipeline
+    val pipeline = new Pipeline()
+      .setStages(Array(tokenizer, remover, cv_text, idf, country_indexer, currency_indexer, encoder, assembler, lr))
+
+    println("Model fitting...")
+
+    // Entrainement du modèle
+    val model = pipeline.fit(trainingData)
+
+    println("Prediction on test data...")
+
+    // Prédictions sur les données test
+    val dfWithSimplePredictions = model.transform(testData)
+    dfWithSimplePredictions.groupBy("final_status", "predictions").count.show()
+
+    val evaluator = new MulticlassClassificationEvaluator()
+      .setLabelCol("final_status")
+      .setPredictionCol("predictions")
+      .setMetricName("f1")
+    val f1_score = evaluator.evaluate(dfWithSimplePredictions)
+    println(s"F1 Score = $f1_score")
 
   }
 }
